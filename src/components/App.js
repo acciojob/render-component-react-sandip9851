61537a4eb638e3a4562e1eb28a39db7ae2201609
sandip <mr.sandippradhan@gmail.com>
2024-07-12 18:17:44 +0530

    finshed

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main">
-      <p>I am learning React. My life is getting better</p>
-    </div>
+    
+      <p> am learning React. My life is getting better.</p>
+    
   )
 }
 
